a4e7bed6082e9d0c5641f2c5ffd9e4697267b589
Sagar Birla <sagarbirla@PAEDLT029.local>
2026-02-19 22:22:48 +0400

    module fmt

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.5.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">= 5.0"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
       version = ">= 4.0"
     }
   }
